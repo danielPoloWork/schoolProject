cb0cc24ec0431a23c2afdacfc400630344e147e1
Daniel Polo <106583643+danielPoloWork@users.noreply.github.com>
2022-07-01 13:03:09 +0200

    Updated Lesson.java

--- src/dataObject/Lesson.java
+++ src/dataObject/Lesson.java
@@ -1,48 +1,51 @@
 package dataObject;
 
+import enumObject.SubjectTypeEnum;
+import java.util.List;
+
 public class Lesson {
 
   // DECLARATION
   private final String id;
   private final String dateTimeEnd;
   private final String dateTimeStart;
-  private final String classId;
-  private final String studentId;
+  private final SubjectTypeEnum subject;
+  private final List<String> students;
   private final String teacherId;
 
   // CONSTRUCTOR
-  public Lesson(String idArg, String dateTimeEndArg, String dateTimeStartArg, String classIdArg,
-      String studentIdArg, String teacherIdArg) {
+  public Lesson(String idArg, String dateTimeEndArg, String dateTimeStartArg, SubjectTypeEnum subjectArg,
+      List<String> studentsArg, String teacherIdArg) {
     this.id = idArg;
     this.dateTimeEnd = dateTimeEndArg;
     this.dateTimeStart = dateTimeStartArg;
-    this.classId = classIdArg;
-    this.studentId = studentIdArg;
+    this.subject = subjectArg;
+    this.students = studentsArg;
     this.teacherId = teacherIdArg;
   }
 
   // SELECT
-  public String selectClassId() {
-    return this.classId;
-  }
-
-  public String selectDateTimeEnd() {
+  public String getDateTimeEnd() {
     return this.dateTimeEnd;
   }
 
-  public String selectDateTimeStart() {
+  public String getDateTimeStart() {
     return this.dateTimeStart;
   }
 
-  public String selectId() {
+  public String getId() {
     return this.id;
   }
 
-  public String selectStudentId() {
-    return this.studentId;
+  public List<String> getStudents() {
+    return this.students;
   }
 
-  public String selectTeacherId() {
+  public SubjectTypeEnum getSubject() {
+    return this.subject;
+  }
+
+  public String getTeacherId() {
     return this.teacherId;
   }
 }
